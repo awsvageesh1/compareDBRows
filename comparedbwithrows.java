7a1f9eb5ad07f406f65255d76398ccdcbb22a27e
awsvageesh1 <146157058+awsvageesh1@users.noreply.github.com>
2023-09-27 11:41:59 -0400

    Update comparedbwithrows.java

--- comparedbwithrows.java
+++ comparedbwithrows.java
@@ -26,34 +26,41 @@ public class DatabaseComparison {
                 }
 
                 String accountID = row.getCell(0).getStringCellValue();
-                String roleCodesStr = row.getCell(1).getStringCellValue();
-                
-                // Split the role codes by comma
-                String[] roleCodesStrArray = roleCodesStr.split(",");
-                
-                for (String roleCodeStr : roleCodesStrArray) {
-                    int roleCode = Integer.parseInt(roleCodeStr.trim());
-                    
-                    // Construct SQL query
-                    String query = "SELECT * FROM table_amenities WHERE accountid = ? AND rolecode = ?";
+                int roleCode;
+                Cell roleCodeCell = row.getCell(1);
 
-                    // Execute queries on the first database (Sybase)
-                    try (PreparedStatement statement1 = connection1.prepareStatement(query)) {
-                        statement1.setString(1, accountID);
-                        statement1.setInt(2, roleCode);
+                // Check cell type and extract the role code accordingly
+                if (roleCodeCell.getCellType() == CellType.STRING) {
+                    // Role code is stored as a string
+                    roleCode = Integer.parseInt(roleCodeCell.getStringCellValue().trim());
+                } else if (roleCodeCell.getCellType() == CellType.NUMERIC) {
+                    // Role code is stored as a numeric value
+                    roleCode = (int) roleCodeCell.getNumericCellValue();
+                } else {
+                    // Handle other cell types or errors as needed
+                    System.err.println("Unsupported cell type for role code.");
+                    continue;
+                }
 
-                        ResultSet resultSet1 = statement1.executeQuery();
-                        // Process and compare results as needed
-                    }
+                // Construct SQL query
+                String query = "SELECT * FROM table_amenities WHERE accountid = ? AND rolecode = ?";
 
-                    // Execute queries on the second database (DB2)
-                    try (PreparedStatement statement2 = connection2.prepareStatement(query)) {
-                        statement2.setString(1, accountID);
-                        statement2.setInt(2, roleCode);
+                // Execute queries on the first database (Sybase)
+                try (PreparedStatement statement1 = connection1.prepareStatement(query)) {
+                    statement1.setString(1, accountID);
+                    statement1.setInt(2, roleCode);
 
-                        ResultSet resultSet2 = statement2.executeQuery();
-                        // Process and compare results as needed
-                    }
+                    ResultSet resultSet1 = statement1.executeQuery();
+                    // Process and compare results as needed
+                }
+
+                // Execute queries on the second database (DB2)
+                try (PreparedStatement statement2 = connection2.prepareStatement(query)) {
+                    statement2.setString(1, accountID);
+                    statement2.setInt(2, roleCode);
+
+                    ResultSet resultSet2 = statement2.executeQuery();
+                    // Process and compare results as needed
                 }
             }
 
